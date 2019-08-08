b43829ebf70874881992614f720d62dcd93f0537
Peter Wrenn <18408058+pwrenn@users.noreply.github.com>
2019-08-07 17:34:23 -0700

    Setting up json-server backend and scaffolding for front end

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+	useEffect(() => {
+		// Initializes Materialize JS
+		M.AutoInit();
+	});
+
+	return <div className='App'>Hello World</div>;
+};
+
+export default App;
